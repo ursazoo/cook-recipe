bb04149db51eaca49a610cf34cdf8604cf99d5b8
秦锋 <iamzhoufei@gmail.com>
2023-06-13 04:50:38 +0800

    feat: init project

---
+++ src/views/HomeView.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+import TheWelcome from '../components/TheWelcome.vue'
+</script>
+
+<template>
+  <main>
+    <TheWelcome />
+    <a-button type="primary">一个按钮</a-button>
+  </main>
+</template>
