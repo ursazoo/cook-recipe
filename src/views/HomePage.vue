96879094a7bcc4625f8f4523e131e98103e920f8
秦锋 <iamzhoufei@gmail.com>
2023-06-14 12:54:44 +0800

    feat: add router

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -1,12 +1,15 @@
 <template>
   <main>
     <a-button type="primary" @click="modalStore.isSignInModalShow = true">一个按钮</a-button>
-    <a-button type="primary" @click="modalStore.isSignInModalShow = false">关闭</a-button>
+    <a-button type="primary" @click="router.push('/about')">关闭</a-button>
   </main>
 </template>
 
 <script setup lang="ts">
 import { useModalStore } from "@/stores/modal";
+import { useRouter } from "vue-router";
+
 const modalStore = useModalStore();
+const router = useRouter();
 
 </script>
