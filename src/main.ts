131e4b499b6e3517610efde561b8392c4503aad1
秦锋 <iamzhoufei@gmail.com>
2023-06-18 02:29:58 +0800

    feat: add lottie web animation json

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import Vue3Lottie from 'vue3-lottie'
 import 'virtual:svg-icons-register'
 
 import ArcoVueIcon from '@arco-design/web-vue/es/icon';
@@ -7,6 +8,7 @@ import ArcoVueIcon from '@arco-design/web-vue/es/icon';
 import App from './App.vue'
 import router from './router'
 
+import 'vue3-lottie/dist/style.css'
 import '@/styles/global.less';
 import '@arco-design/web-vue/dist/arco.css';
 
@@ -15,5 +17,6 @@ const app = createApp(App)
 app.use(createPinia())
 app.use(router)
 app.use(ArcoVueIcon);
+app.use(Vue3Lottie);
 
 app.mount('#app')
