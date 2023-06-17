1c96d0bd8357f3ecff5ec39832313504dee5d01c
秦锋 <iamzhoufei@gmail.com>
2023-06-18 02:33:35 +0800

    feat: init recipe page

---
+++ src/views/RecipePage.vue
@@ -0,0 +1,21 @@
+<template>
+<div class="recipe-page-container">
+  {{recipeName}}
+</div>
+</template>
+
+<script setup lang="ts">
+import { useRouteParams } from '@vueuse/router'
+
+const recipeName = useRouteParams('recipeName')
+</script>
+
+<style lang="less" scoped>
+.recipe-page-container {
+  border-radius: 15px;
+  padding: 30px 15px;
+  background: #fff;
+  box-shadow: 0 1px 5px 1px rgba(0, 0, 0, 0.1);
+}
+
+</style>
