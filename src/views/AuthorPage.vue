8d51ecdc47feae60802db029df02ee80d616dc91
秦锋 <iamzhoufei@gmail.com>
2023-06-18 02:33:41 +0800

    feat: init author page

---
+++ src/views/AuthorPage.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="author-page-container"></div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style lang="less" scoped>
+.author-page-container {
+  border-radius: 15px;
+  padding: 30px 0;
+  background: #fff;
+  box-shadow: 0 1px 5px 1px rgba(0, 0, 0, 0.1);
+}
+
+</style>
