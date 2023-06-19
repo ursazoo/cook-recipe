21a274ccef6d25fc9b0ec37ac13f21fd53ecc6ff
秦锋 <iamzhoufei@gmail.com>
2023-06-19 11:51:14 +0800

    feat: update App.vue structure

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,15 @@
-<script setup lang="ts">
-import { RouterView } from 'vue-router'
-import BaseSignInModal from "@/components/BaseSignInModal.vue";
-import BaseLayout from "@/layout/BaseLayout.vue";
-</script>
-
 <template>
   <BaseLayout>
     <RouterView />
   </BaseLayout>
   <BaseSignInModal />
 </template>
+
+<script setup lang="ts">
+import { RouterView } from 'vue-router'
+import BaseSignInModal from "@/components/BaseSignInModal.vue";
+import BaseLayout from "@/layout/BaseLayout.vue";
+</script>
 
 <style scoped>
 </style>
